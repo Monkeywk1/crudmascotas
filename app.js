815778f70fda3b6af189813df6910e482295b142
David Alzate <david.alzate777@gmail.com>
2024-07-29 01:14:15 -0500

    Barra de navegacion y enritamiento

--- app.js
+++ app.js
@@ -9,18 +9,8 @@ app.set('views', __dirname + '/views');
 /*middleware para contenido estatico */
 app.use(express.static(__dirname + '/public'));
 
-/*enrutamiento */
-
-app.get('/', (req, res) => {
-  //res.send('Hello World!')
-  res.render('index', {titulo: "Bienvenido a Node.js con Express y con EJS"});
-})
-
-app.get('/servicios',(req, res)=>{
-    //res.send("Bienvenido a la pagina de servicios");
-    res.render('servicios', {titulo: "Nuestros Servicios Dínamicos"});
-
-});
+/*rutas web */
+app.use('/', require('./router/rutasweb'));
 
 /*direccionar a vista 404 cuando se presente el error404 */
 app.use((req, res, next) => {
---
+++ router/rutasweb.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router();
+
+/*enrutamiento */
+
+router.get('/', (req, res) => {
+    res.render('index', {titulo: "Bienvenido a Node.js con Express y con EJS"});
+})
+  
+router.get('/servicios',(req, res)=>{
+    res.render('servicios', {titulo: "Nuestros Servicios Dínamicos"});
+});
+
+module.exports = router;
