ccb2316c3ee8d8c375fc196db7f997b6390734ed
David Alzate <david.alzate777@gmail.com>
2024-07-30 01:12:34 -0500

    Vista de mascotas dinamica

--- router/mascotas.js
+++ router/mascotas.js
@@ -2,7 +2,12 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/',(req, res)=>{
-    res.render('mascotas', {titulo: "Página de Mascotas"});
+    res.render('mascotas', {arrayMascotas: [
+        {id: 'mas001', nombre: 'Coco', descripcion: 'Perro French Poodle'},
+        {id: 'mas001', nombre: 'Endy', descripcion: 'Gato Persa'},
+        {id: 'mas001', nombre: 'Duna', descripcion: 'Perro Labrador'},
+        {id: 'mas001', nombre: 'Grays', descripcion: 'Gato Angora'},
+    ]});
 });
 
 module.exports = router;
