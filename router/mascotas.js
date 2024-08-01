6673cd63443626e153577f4f10f2be9f12d4fff2
David Alzate <david.alzate777@gmail.com>
2024-08-01 08:40:02 -0500

    consulta de datos a la db Mongo

--- router/mascotas.js
+++ router/mascotas.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const router = express.Router();
 
-router.get('/',(req, res)=>{
-    res.render('mascotas', {arrayMascotas: [
-        {id: 'mas001', nombre: 'Coco', descripcion: 'Perro French Poodle'},
-        {id: 'mas001', nombre: 'Endy', descripcion: 'Gato Persa'},
-        {id: 'mas001', nombre: 'Duna', descripcion: 'Perro Labrador'},
-        {id: 'mas001', nombre: 'Grays', descripcion: 'Gato Angora'},
-    ]});
+const Mascota = require('../models/mascota');
+
+router.get('/', async (req, res)=>{
+try {
+    const arrayMascotas = await Mascota.find();
+//    console.log(arrayMascotas)
+    res.render("mascotas",{arrayMascotas})
+} catch (error) {
+    console.log(error)
+}
+
+//    res.render('mascotas', {arrayMascotas: [
+//        {id: 'mas001', nombre: 'Coco', descripcion: 'Perro French Poodle'},
+//        {id: 'mas001', nombre: 'Endy', descripcion: 'Gato Persa'},
+//        {id: 'mas001', nombre: 'Duna', descripcion: 'Perro Labrador'},
+//        {id: 'mas001', nombre: 'Grays', descripcion: 'Gato Angora'},
+//    ]});
 });
 
 module.exports = router;
