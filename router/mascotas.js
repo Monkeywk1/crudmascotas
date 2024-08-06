391fb1e7754b745532e1c98edc788420b1910d24
David Alzate <david.alzate777@gmail.com>
2024-08-06 00:00:32 -0500

    CRUD inicial mascotas

--- router/mascotas.js
+++ router/mascotas.js
@@ -12,12 +12,61 @@ try {
     console.log(error)
 }
 
-//    res.render('mascotas', {arrayMascotas: [
-//        {id: 'mas001', nombre: 'Coco', descripcion: 'Perro French Poodle'},
-//        {id: 'mas001', nombre: 'Endy', descripcion: 'Gato Persa'},
-//        {id: 'mas001', nombre: 'Duna', descripcion: 'Perro Labrador'},
-//        {id: 'mas001', nombre: 'Grays', descripcion: 'Gato Angora'},
-//    ]});
 });
 
+router.get('/crear', (req, res)=>{
+        res.render('crear');
+});
+
+/*router para recibir datos del formulario crear */
+router.post('/', async (req, res)=>{
+    const body = req.body;
+//    console.log(body);
+    try {
+        await Mascota.create(body)
+        res.redirect('/mascotas')
+    } catch (error){
+        console.log('error:', error)
+    }
+});
+/*router para editar un documento */
+router.get("/:id", async (req, res)=>{
+    const id = req.params.id;
+    try {
+        const mascotaBD = await Mascota.findOne({_id: id})
+        //console.log(mascotaDB)
+        res.render('detalle',{
+            mascota: mascotaBD,
+            error: false
+        })
+    } catch (error){
+        console.log('error:', error)
+        res.render('detalle',{
+            error: true,
+            mensaje: "No se encontro ningún registro qué conincida con el id"
+        })
+    }
+});
+/*router para borrar un documento */
+router.delete("/:id", async (req, res)=>{
+    const id = req.params.id;
+    try {
+        const mascotaDB = await Mascota.findByIdAndDelete({_id: id})
+        if (!mascotaDB) {
+            res.json({
+                estado: false,
+                mensaje: "No fue posible eliminar el registro"
+            })
+        } else {
+            res.json({
+                estado: true,
+                mensaje: "Registro Eliminado!!!"
+        })
+        }
+        
+    } catch (error){
+        console.log('error:', error)
+        
+    }
+});
 module.exports = router;
